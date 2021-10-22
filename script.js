9602def59e59fb00d5eb6c05815380e67214f81a
Eli Pagan <mbk8689@gmail.com>
2021-10-22 18:29:55 -0400

    added prompts

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//var numbers = '0123456789';
+//var symbols = "!@#$%^&*_-+=?";
+
+
+
+
 // Assignment code here
 
 
@@ -6,11 +13,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var promptLength = window.prompt("How long do you want your password to be? Password must be between 8 and 20 chartacters.")
+  
+  var promptCharacters = window.prompt("What type of characters would you like to choose?")
+
+
+  
+  
+ 
+
+  var password = "generatePassword"
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
