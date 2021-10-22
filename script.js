fe13bce5e6c09218f1e83ead601b51fc3ab7f1d5
Eli Pagan <mbk8689@gmail.com>
2021-10-22 19:24:13 -0400

    im lost

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//var numbers = '0123456789';
-//var symbols = "!@#$%^&*_-+=?";
-
-
-
-
 // Assignment code here
 
 
@@ -13,17 +6,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
-  var promptLength = window.prompt("How long do you want your password to be? Password must be between 8 and 20 chartacters.")
-  
-  var promptCharacters = window.prompt("What type of characters would you like to choose?")
-
-
-  
   
  
 
-  var password = "generatePassword"
+  var password = generatePassword;
 
 
   var passwordText = document.querySelector("#password");
