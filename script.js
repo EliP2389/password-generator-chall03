2599b1b8580d7ae5c8d2e8174d0142ad5bed12e5
Eli Pagan <mbk8689@gmail.com>
2021-10-23 21:19:28 -0400

    added stuff, still figuring it out

--- script.js
+++ script.js
@@ -10,10 +10,10 @@
 
 //0,1,2,3,4,5,6,7,8,9,
 
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lower = "abcdefghijklmnopqrstuvwxyz";
-const special = "!@#$%^&*()-=+_{}";
-const numbers = "0123456789";
+//const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//const lower = "abcdefghijklmnopqrstuvwxyz";
+//const special = "!@#$%^&*()-=+_{}";
+//const numbers = "0123456789";
 
 //generate password
 function generatePassword() {
@@ -26,10 +26,26 @@ if(passwordLength >= 8 && passwordLength <= 128){
   alert("Your password must be between 8 - 128 characters long")
 } 
 
+var characters = {
+  upper: window.confirm("Would you like to include Upper Case letters?"),
+  lower: window.confirm("Would you like to include Lower Case letters?"),
+  special: window.confirm("Would you like to include Special Characters?"),
+  numbers: window.confirm("Would you like to include numbers?")
+}
 
+if(characters.upper === false && characters.lower === false 
+  && characters.special === false && characters.numbers === false ){
+    window.alert("No characters chosen")
+  }
 
 
 //function to generate random
+for (var i = 0; i <= passwordLength; i++)
+
+if (passwordLength >= 8) {
+
+}
+
 var randomPassword = function(min, max){
   var value = Math.floor(Math.random() * (max - min + 1) + min)
 
@@ -42,12 +58,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  
- 
-
   var password = generatePassword();
-
-
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
